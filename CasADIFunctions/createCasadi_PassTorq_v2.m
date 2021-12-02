114c6c73650a1f65913d821789ef7b4d00220863
Dhruv Gupta <dhruv.gupta@kuleuven.be>
2021-12-02 16:01:45 +0100

    Update on createCasadi_PassTorq
    Need some parameters defined in the beginning of the function

--- CasADIFunctions/createCasadi_PassTorq_v2.m
+++ CasADIFunctions/createCasadi_PassTorq_v2.m
@@ -48,26 +48,28 @@ f_passiveTATorques = Function('f_passiveTATorques',{stiff,damp,qin,qdotin}, ...
 %% Passive joint torques
 % We extract the parameters for the passive torques of the lower limbs and
 % the trunk
-k_pass.hip.flex = [-2.44 5.05 1.51 -21.88]';
-theta.pass.hip.flex = [-0.6981 1.81]';
-k_pass.hip.add = [-0.03 14.94 0.03 -14.94]';
-theta.pass.hip.add = [-0.5 0.5]';
-k_pass.hip.rot = [-0.03 14.94 0.03 -14.94]';
-theta.pass.hip.rot = [-0.92 0.92]';
-k_pass.knee = [-6.09 33.94 11.03 -11.33]';
-theta.pass.knee = [-2.4 0.13]';
-k_pass.ankle = [-2.03 38.11 0.18 -12.12]';
-theta.pass.ankle = [-0.74 0.52]';
-k_pass.subt = [-60.21 16.32 60.21 -16.32]';
-theta.pass.subt = [-0.65 0.65]';
-k_pass.mtp = [-0.9 14.87 0.18 -70.08]';
-theta.pass.mtp = [0 65/180*pi]';
-k_pass.trunk.ext = [-0.35 30.72 0.25 -20.36]';
-theta.pass.trunk.ext = [-0.5235987755982988 0.17]';
-k_pass.trunk.ben = [-0.25 20.36 0.25 -20.36]';
-theta.pass.trunk.ben = [-0.3490658503988659 0.3490658503988659]';
-k_pass.trunk.rot = [-0.25 20.36 0.25 -20.36]';
-theta.pass.trunk.rot = [-0.3490658503988659 0.3490658503988659]';
+
+% NEED TO CHANGE THEM IN THE BEGINNING, AND CALL the muscle actuated joints as ExtFunIO.jointi.names.muscleActuated
+% k_pass.hip.flex = [-2.44 5.05 1.51 -21.88]';
+% theta.pass.hip.flex = [-0.6981 1.81]';
+% k_pass.hip.add = [-0.03 14.94 0.03 -14.94]';
+% theta.pass.hip.add = [-0.5 0.5]';
+% k_pass.hip.rot = [-0.03 14.94 0.03 -14.94]';
+% theta.pass.hip.rot = [-0.92 0.92]';
+% k_pass.knee = [-6.09 33.94 11.03 -11.33]';
+% theta.pass.knee = [-2.4 0.13]';
+% k_pass.ankle = [-2.03 38.11 0.18 -12.12]';
+% theta.pass.ankle = [-0.74 0.52]';
+% k_pass.subt = [-60.21 16.32 60.21 -16.32]';
+% theta.pass.subt = [-0.65 0.65]';
+% k_pass.mtp = [-0.9 14.87 0.18 -70.08]';
+% theta.pass.mtp = [0 65/180*pi]';
+% k_pass.trunk.ext = [-0.35 30.72 0.25 -20.36]';
+% theta.pass.trunk.ext = [-0.5235987755982988 0.17]';
+% k_pass.trunk.ben = [-0.25 20.36 0.25 -20.36]';
+% theta.pass.trunk.ben = [-0.3490658503988659 0.3490658503988659]';
+% k_pass.trunk.rot = [-0.25 20.36 0.25 -20.36]';
+% theta.pass.trunk.rot = [-0.3490658503988659 0.3490658503988659]';
 
 %% Create function to compute passive moments
 
@@ -83,8 +85,8 @@ Tau_passj_muscleActuated = [];
 for i=1:nq.muscAct
 
 %   coord_muscleActuated(i)
-    Tau_passj_muscleActuated_var    = f_PassiveMoments(k_pass.hip.flex,...
-    theta.pass.hip.flex,Q_SX(coord_muscleActuated(i)),...
+    Tau_passj_muscleActuated_var    = f_PassiveMoments(k_pass.(model_info.ExtFunIO.jointi.names.muscleActuated{i}),...
+    theta.pass.(model_info.ExtFunIO.jointi.names.muscleActuated{i}),Q_SX(coord_muscleActuated(i)),...
     Qdot_SX(coord_muscleActuated(i)));
     Tau_passj_muscleActuated = [Tau_passj_muscleActuated; Tau_passj_muscleActuated_var];
 
@@ -135,6 +137,7 @@ for i=1:nq.muscAct
 %     Qdot_SX(jointi.trunk.rot));
 end
 
+% NEED TO ADD ARM STIFFNESS AND DAMPING IN THE BEGINNING
 Tau_passj_arms = [];
 
 for i=1:nq.arms
@@ -163,6 +166,8 @@ Tau_passj_arms = [Tau_passj_arms; Tau_passj_arms_var];
 %     Tau_passj.sh_rot.l, Tau_passj.sh_flex.r, Tau_passj.sh_add.r, ...
 %     Tau_passj.sh_rot.r, Tau_passj.elb.l, Tau_passj.elb.r];
 end
+
+% NEED TO ADD MTP STIFFNESS AND DAMPING IN THE BEGINNING
 
 Tau_passj_mtp = [];
 for i=nq.mtp
