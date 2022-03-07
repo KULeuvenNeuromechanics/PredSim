184ae0d79f1d1afccb6a07cd20520b0dcd55bd71
Lars D'Hondt <71920801+Lars-DHondt-KUL@users.noreply.github.com>
2022-03-07 14:46:48 +0100

    Update PreProcessing.m

--- PreProcessing/PreProcessing.m
+++ PreProcessing/PreProcessing.m
@@ -1,41 +1,19 @@
-function [S,model_info] = PreProcessing(S,osim_path)
-%
-% Prepare the information that is required for the problem formulation.
-%
-% Author: Lars D'Hondt
-%
-% Date: 18/January/2022
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [S,model_info]=preprocessing(S,osim_path)
 
-%% subject specific folder
-% to store information about model
-if ~isfolder(S.misc.subject_folder)
-    mkdir(S.misc.subject_folder);
-end
+[]=osim2dll(S,osim_path);
 
-%% generate external function
-% skeletal and contact dynamics, compiled based on .osim file
-% https://github.com/Lars-DHondt-KUL/opensimAD
-ext_fun_path = fullfile(S.misc.subject_folder,['F_' osim_file_name '.dll']);
-if ~exist(ext_fun_path,'file')
-    osim2dll(S,osim_path);
-end
-
-%% create model_info
-% book-keeping
 model_info = get_model_info(S,osim_path);
 
-%% read Muscle-Tendon parameters, and scale them if needed
-% lMo, vMo, FMo, alphao, lTs
-model_info = read_and_scale_MTparameters(S,osim_path,model_info);
+model_info.muscle_info.params = read_and_scale_MTparameters(S,osim_path,model_info);
 
-%% analyse musculo-skeletal geometry, and fit an expression
-% based on OpenSim muscle analysis tool
-get_musculoskeletal_geometry_approximation(S,osim_path,model_info);
+IOfields = fields(model_info.ExtFunIO);
+for i=1:length(IOfields)
+    model_info.ExtFunIO.(IOfields{i}) = convert2double(model_info.ExtFunIO.(IOfields{i}));
+end
+[model_info] = GetIndexHelper(S,model_info);
 
-%% create additional fields with information that are needed later
-%
-model_info = update_model_info(model_info);
+[model_info] = scale_MTparameters(S,model_info);
 
+[model_info] = get_musculoskeletal_geometry_approximation(S,osim_path,model_info);
 
+model_info = update_model_info_muscleProperties(model_info);
