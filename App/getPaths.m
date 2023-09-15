758badb8864c6f88de074ed8fcbcd12c727ce74c
VDB-Bram <bramvandenbosch@outlook.com>
2023-09-15 11:30:47 +0200

    update getPaths

--- App/getPaths.m
+++ App/getPaths.m
@@ -7,7 +7,7 @@ function [init_path_savefolder,init_path_geom,init_path_casadi] = getPaths()
         init_path_geom = 'C:\GBW_MyPrograms\OpenSim 4.3\Geometry';
         init_path_casadi = 'C:\GBW_MyPrograms\casadi_3_5_5';
     elseif strcmp(name, 'GBW-L-W2075')
-        init_path_savefolder = 'C:\Users\u0138016\OneDrive - KU Leuven\Outreach\Kinderuniversiteit\2022\Resultaten';
+        init_path_savefolder = 'C:\Users\u0138016\OneDrive - KU Leuven\Outreach\Startersdagen\2023\Resultaten_VitruvianMan';
         init_path_geom = 'C:\GBW_MyPrograms\OpenSim 4.3\Geometry';
         init_path_casadi = 'C:\GBW_MyPrograms\casadi_3_5_5';
     else
