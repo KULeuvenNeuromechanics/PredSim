9c6285c4e4c6c98872a2f5ad1772dcbde8053452
Lars D'Hondt <71920801+Lars-DHondt-KUL@users.noreply.github.com>
2024-07-17 09:41:29 +0200

    update models and defaults

--- Subjects/DHondt_et_al_2024_3seg/settings_DHondt_et_al_2024_3seg.m
+++ Subjects/DHondt_et_al_2024_3seg/settings_DHondt_et_al_2024_3seg.m
@@ -1,30 +1,37 @@
 % --------------------------------------------------------------------------
-% Settings for DHondt_2023_3seg that deviate from the PredSim defaults
+% Settings for DHondt_et_al_2024_3seg that deviate from the PredSim defaults
 %
-%   L. D'Hondt, F. D. Groote, and M. Afschrift, "A dynamic foot model for 
-%   predictive simulations of gait reveals causal relations between foot 
-%   structure and whole body mechanics.” bioRxiv, p. 2023.03.22.533790, 
-%   Mar. 24, 2023. doi: 10.1101/2023.03.22.533790.
+%   L. D’Hondt, F. D. Groote, and M. Afschrift, “A dynamic foot model for 
+%   predictive simulations of human gait reveals causal relations between 
+%   foot structure and whole-body mechanics,” PLOS Computational Biology, 
+%   vol. 20, no. 6, p. e1012219, Jun. 2024, doi: 10.1371/journal.pcbi.1012219.
 %
 % Original author: Lars D'Hondt
-% Original date: 11/December/2023
+% Original date: 16/July/2024
 % --------------------------------------------------------------------------
 
-S.subject.name = 'DHondt_2023_3seg';
+S.subject.name = 'DHondt_et_al_2024_3seg';
 
 S.subject.v_pelvis_x_trgt   = 1.33;
 
 S.solver.N_meshes = 100;
 
-S.subject.IG_selection_gaitCyclePercent = 100;
 S.subject.IG_selection = 'quasi-random';
+S.subject.IG_pelvis_y = 0.9385;
+S.subject.adapt_IG_pelvis_y = 0;
 
 S.metabolicE.tanh_b = 100;
 
 S.bounds.Qs = {'pelvis_ty',0.55,1.1, 'pelvis_tilt',-2.9302,nan};
 
-S.subject.muscle_pass_stiff_shift = {{'soleus','_gas','per_','tib_','_dig_','_hal_','FDB'},0.9};
+S.subject.mtp_type = '2022paper';
+S.subject.set_stiffness_coefficient_selected_dofs = {'mtp_angle',25};
+S.subject.set_damping_coefficient_selected_dofs = {'mtp_angle',2};
+
+S.subject.muscle_pass_stiff_shift =...
+    {{'soleus','_gas','per_','tib_','_dig_','_hal_','FDB'},0.9};
 S.subject.tendon_stiff_scale = {{'soleus','_gas'},0.5};
+
 
 
 % to prevent body segments from clipping into eachother
@@ -57,6 +64,9 @@ S.bounds.distanceConstraints(5).point2 = 'toes_l';
 S.bounds.distanceConstraints(5).direction = 'xz';
 S.bounds.distanceConstraints(5).lower_bound = 0.1;
 S.bounds.distanceConstraints(5).upper_bound = 2;
+
+
+
 
 
 
