df4ab6b52bf37ba264098d0a7eab2aba1cb6f2dc
miriamfebrer <miriam.febrer@upc.edu>
2024-09-16 14:53:23 +0200

    Update synergies.m
    Add and document an example for running a simulation with muscle syenrgies

--- Examples/synergies.m
+++ Examples/synergies.m
@@ -1,12 +1,25 @@
 % --------------------------------------------------------------------------
-% synergies
-%   Explanation of what this function does. Length depends on function 
-%   complexity. Include relevant citations. If applicable, provide an
-%   example and/or refer to a unit test. 
-%
+% Implementation of motor control modelled through muscle synergies
 % 
-% Original author: (First name Last name)
-% Original date: (Using "30/May/2022" format avoids confusion)
+%   Here we provide an example of how to define the settings to run a
+%   predictive simulation using muscle synergies to model motor control. 
+% 
+%   In this specific example, we run a simulation of a half gait cycle (symmetric)
+%   with a 2D model, imposing four synergies per leg, and tracking synergy
+%   weights. The values for muscle weights have been obtained from the figures 
+%   from Pitto et al. 2020, and correspond to the generic weights for four 
+%   synergies of a group of children with cerebral palsy.
+% 
+%   Pitto L, van Rossom S, Desloovere K, Molenaers G, Huenaerts C, et al. (2020) 
+%   Pre-treatment EMG can be used to model post-treatment muscle coordination 
+%   during walking in children with cerebral palsy. PLOS ONE 15(2): e0228851. 
+%   https://doi.org/10.1371/journal.pone.0228851
+%
+%   The full description of the different settings is given in 
+%   Documentation/SettingsOverview.md
+% 
+% Original author: Míriam Febrer-Nafría
+% Original date: 16/Sept/2024
 % --------------------------------------------------------------------------
 
 clear
@@ -22,12 +35,12 @@ addpath(pathRepo)
 
 %% Initialize S
 
-[S] = initializeSettings('Falisse_et_al_2022');
+[S] = initializeSettings('gait1018');
 
 %% Settings
 
 % name of the subject
-S.subject.name = 'Falisse_et_al_2022';
+S.subject.name = 'gait1018';
 
 % path to folder where you want to store the results of the OCP
 S.misc.save_folder  = fullfile(pathExDir,'ExampleResults','Synergies'); 
@@ -47,7 +60,6 @@ S.subject.synergies = 1;
 % Impose 4 synergies for right side (and 4 for left side - symmetric)
 S.subject.NSyn_r = 4;
 
-
 % Track synergy weights
 S.subject.TrackSynW = 1; 
 
@@ -55,18 +67,15 @@ S.subject.TrackSynW = 1;
 S.subject.TrackSynW_NSyn_r = 4; 
 
 % Synergy weights to be tracked
-% (from the case of 4 synergies in Pitto et al 2020)
+% The names of muscles should be updated for each model
 S.subject.knownSynW_r = {'rect_fem_r', [0 0.28 0 0.75],...
-    'vas_lat_r', [0.1 0.1 0.05 0.8],...
-    {'bifemlh_r','bifemsh_r'}, [0.6 0.4 0.15 0.2],...
-    {'semiten_r','semimem_r'}, [0 0 1 0],...
+    'vasti_r', [0.1 0.1 0.05 0.8],...
+    'bifemsh_r', [0.6 0.4 0.15 0.2],...
+    'hamstrings_r', [0 0 1 0],...
     'tib_ant_r', [0.2 1 0.01 0],...
-    {'med_gas_r','lat_gas_r'}, [0.95 0.08 0.04 0.03],...
+    'gastroc_r', [0.95 0.08 0.04 0.03],...
     'soleus_r', [1 0.05 0 0.05],...
-    {'glut_med1_r','glut_med2_r','glut_med3_r'}, [0.15 0 0.04 1]};
-
-
-
+    'glut_max_r', [0.15 0 0.04 1]};
 
 %% Run predictive simulations
 
