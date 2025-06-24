62fdc71809de74780324fb6a3ea04159783948b8
ellisvancan <ellisvancan@live.nl>
2025-06-24 10:00:51 +0200

    Revert "updated readme"
    This reverts commit 795a006bbcedc6e86c42bd9b5a5c88803f540dc2.

--- main.m
+++ main.m
@@ -7,59 +7,36 @@
 clear
 close all
 clc
-
 % path to the repository folder
-pathRepo ='C:\GBW_MyPrograms\sharedPredSim';
+[pathRepo,~,~] = fileparts(mfilename('fullpath'));
 % path to the folder that contains the repository folder
 [pathRepoFolder,~,~] = fileparts(pathRepo);
 
 %% Initialize S
-[S] = initializeSettings('DHondt_et_al_2024_3seg');
-
 addpath(fullfile(pathRepo,'DefaultSettings'))
 
+[S] = initializeSettings('Falisse_et_al_2022');
+
 %% Settings
+
 % name of the subject
-S.subject.name = 'CP_AFO_3';
+S.subject.name = 'Falisse_et_al_2022';
 
 % path to folder where you want to store the results of the OCP
 S.misc.save_folder  = fullfile(pathRepoFolder,'PredSimResults',S.subject.name); 
 
-S.misc.main_path = pathRepo ;
+% either choose "quasi-random" or give the path to a .mot file you want to use as initial guess
+S.solver.IG_selection = fullfile(S.misc.main_path,'OCP','IK_Guess_Full_GC.mot');
+S.solver.IG_selection_gaitCyclePercent = 100;
+% S.solver.IG_selection = 'quasi-random';
 
 % give the path to the osim model of your subject
-osim_path = fullfile(pathRepo,'Subjects',S.subject.name,[S.subject.name '_PredSim.osim']);
-% osim_path = fullfile(pathRepo,'Subjects',S.subject.name,[S.subject.name '.osim']);
-% 
-S.settings.IG = 'ig1';
-S.settings.scaling = 'sc1';
-S.settings.jointparams ='j1';
-S.settings.set_limit_torque_coefficients_selected_dofs = 'limtor1';
-% 
-% %%%%%%
-% % additional
-S.settings.muscle_strength = 'IWA_NOmmt';
-S.settings.scale_MT_params = 'mt2';
-% S.settings.motor_control = 'SynW';
-%%%%
-% ---------------------------------------------------------------
-settings_path = fullfile(pathRepo,'Subjects',S.subject.name,['settings_',S.subject.name,'.m']);
+osim_path = fullfile(pathRepo,'Subjects',S.subject.name,[S.subject.name '.osim']);
 
-% S.post_process.make_plot = 1;
-S.misc.savename  = 'datetime_job';
-S.solver.CasADi_path = 'C:\GBW_MyPrograms\casadi_3_5_5';
-S.solver.par_cluster_name = '6x4';
-S.solver.run_as_batch_job = 1;
-% S.solver.batch_job_paths = {};
-
-
-S = settings_CP_AFO_3(S);
-S.solver.adaptIG = true;
 
 %% Run predictive simulations
 
 [savename] = runPredSim(S, osim_path);
-
 
 
 %% Plot results
