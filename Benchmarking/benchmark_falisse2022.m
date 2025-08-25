1268c05c1352075b43e81aa65d74d7c045660f65
Afschrift <m.a.afschrift@vu.nl>
2025-08-25 16:05:12 +0200

    created benchmark_predsim function

--- Benchmarking/benchmark_falisse2022.m
+++ Benchmarking/benchmark_falisse2022.m
@@ -46,6 +46,15 @@ S.solver.par_cluster_name = 'Cores4';
 % assume that the user wants to benchmark everything (gait speed, slope,
 % walking with added mass).
 
+% benchmark specific studies
+S_benchmark.studies = {'vanderzee2022'};
+% options are:
+%   vanderzee2022: variations in gait speed
+%   koelewijn2019: variation in gait speed and slope
+%   browning: added mass to body segments
+%   schertzer: added mass to body segments and variations in gait speed
+
+
 % benchmark gait speed simulations
 S_benchmark.gait_speeds = True;
 S_benchmark.gait_speed_range = [0.6 2];
@@ -74,72 +83,68 @@ S_benchmark.added_mass_studies = {'Browning','Schertzer','Huang'}; % add studies
 S_benchmark.out_folder = fullfile(pathRepo,'Results','Benchmark_Falisse2022');
 
 
-%% Here I will start the function benchmarking
+%% Run benchmarking procedure
 
-% The only input is S, maybe additional input S_benchmark
-S_input = S; % make copy of S
-
-% get path information
+% matlab function used to start all simulations
+benchmark_predsim(S,osim_path,S_benchmark);
 
 
-% flow of the function
-%-----------------------
-% 0. pre-processing default model
-%       I think the most clean way is to add additional settings to control
-%       the flow in the run_pred_sim script
-S.pre_processing_only = True; 
-run_pred_sim(S,osim_path);
-S.pre_processing_only = False;
-
-%
-% 1. check if we have to make multiple .dll files
-%    (this is needed for walking on a slope and with added mass)
-%    if this is the case:
-%       - make new .osim files
-%       - create new .dll files
-%       - copy preprocessing information from default model to new models
-%           - F_Falisse_et_al_2022_IO.mat
-%           - f_lMT_vMT_dM_poly_3_9
-
-% 2. Run batch simulations
-
-% batch simulation gait speeds
-if S_benchmark.gait_speeds
-    % get set of gait speeds to test
-    if isfield(S_benchmark,'gait_speeds_selection') && ...
-            ~isempty(S_benchmark.gait_speeds_selection)
-        gait_speeds = S_benchmark.gait_speeds_selection;
-    else
-        if isfield(S_benchmark,'gait_speed_range') && ...
-                ~isempty(S_benchmark.gait_speed_range)
-            speed_min = min(S_benchmark.gait_speed_range);
-            speed_max = max(S_benchmark.gait_speed_range);
-            gait_speeds = speed_min:0.1:speed_max;
-        end
-    end
-    if isempty(gait_speeds)
-        gait_speeds = 0.6:0.1:2;
-    end
-
-    % run predictive simulations
-    for i_speed = 1:length(gait_speeds)
-
-    end
+%% Compare simulation with experiment
 
 
-
-
-end
 
 
 % 3. method to attach data from literature to each simulation or make a
 % table as in my script TableSim.m (benchmarking study). Not sure how to do
 % this properply.
 %
-% Other option to only provide a graphical benchmarking. In other words
+% Other option is to only provide a graphical benchmarking. In other words
 % only make a figure with measured and simulated values. For each
 % experimental condition look for the datapoint that is most similar to 
 % measured values
+%
+% current idea is to write function here that attaches experimental data to
+% simulation results structure. This is only for the replication of
+% specific studies. I you want to benchmark generic effects of gait speed I
+% will only add the figure (e.g. measured and simulated COT as a function
+% of gait speed).
+% this is a seperate function that you should run when all simulations are
+% finished
+
+% step 0. download data from Benchmarking paper 2025:
+% DOI 10.5281/zenodo.14524619
+data_exp_path = fullfile(S_benchmark.out_folder,'exp_data');
+if ~isfolder(data_exp_path)
+    mkdir(data_exp_path);
+end
+
+% general data
+LinkZipFile = 'https://zenodo.org/records/14524620/files/PredSim_gait_conditions-master_19_12_2024.zip?download=1';
+websave(fullfile(data_exp_path,'githubrepo_data.zip'),LinkZipFile);
+unzip(fullfile(data_exp_path,'githubrepo_data.zip'),fullfile(data_exp_path,'github_repo_temp'));
+delete(fullfile(data_exp_path,'githubrepo_data.zip'));
+%       change data format a bit here, we don't need everything
+copyfile(fullfile(data_exp_path,'github_repo_temp','PredSim_gait_conditions-master','ExperimentalData'),...
+    fullfile(data_exp_path,'general_exp_data'))
+rmdir(fullfile(data_exp_path,'github_repo_temp'),'s');
+
+% Van Der Zee kinematic data
+if isfield(S_benchmark,'studies') && ~isempty(S_benchmark.studies)
+    if any(strcmp(S_benchmark.studies,'vanderzee2022'))
+        LinkZipFile = 'https://zenodo.org/records/14524620/files/VanDerZee_Averages_Addbiomech.zip?download=1';
+        websave(fullfile(data_exp_path,'vanderzee2022.zip'),LinkZipFile);
+        unzip(fullfile(data_exp_path,'vanderzee2022.zip'),fullfile(data_exp_path,'vanderzee2022'));
+        delete(fullfile(data_exp_path,'vanderzee2022.zip'));
+    end
+    % attach experimental data to simulation results
+    
+end
+
+
+
+
+
+
 
 % 4. (optional) make figures as in 2022 paper (or some of the figures)
 
