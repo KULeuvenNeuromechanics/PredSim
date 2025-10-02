89c7784688706bf059d64e1b1c77ed5567f92052
WillaertJente <jente.willaert@kuleuven.be>
2025-10-02 14:29:16 +0200

    Aanpassingen app zwaartekracht

--- App2025/getPaths.m
+++ App2025/getPaths.m
@@ -14,6 +14,10 @@ function [init_path_savefolder,init_path_geom,init_path_casadi] = getPaths()
         init_path_savefolder = 'C:\GBW_MyPrograms\KinderuniversiteitApp\Resultaten';
         init_path_geom = 'C:\OpenSim 4.4\Geometry';
         init_path_casadi = 'C:\GBW_MyPrograms\casadi_3_5_5';
+    elseif strcmp(name,'GBW-L-W4394')
+        init_path_savefolder = 'C:\Users\u0125183\OneDrive - KU Leuven\Kinderuniversiteit';
+        init_path_geom = 'C:\GBW_MyPrograms\OpenSim 4.5\Geometry';
+        init_path_casadi = 'C:\GBW_MyPrograms\Casadi';
     % elseif strcmp(name,'MSI')
     %     init_path_savefolder = 'D:\OneDrive - KU Leuven\Results_PredSim_App\Testing';
     %     init_path_geom = 'D:\software\OpenSim 4.3\Geometry';
