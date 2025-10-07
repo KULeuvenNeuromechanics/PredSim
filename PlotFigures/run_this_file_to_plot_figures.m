477efd197c2288b575fd92361755ef9700a5c038
Lars D'Hondt <71920801+Lars-DHondt-KUL@users.noreply.github.com>
2025-10-07 14:20:27 +0200

    Update run_this_file_to_plot_figures.m

--- PlotFigures/run_this_file_to_plot_figures.m
+++ PlotFigures/run_this_file_to_plot_figures.m
@@ -19,7 +19,7 @@ result_paths{2} = fullfile(results_folder,'DHondt_et_al_2024_4seg','DHondt_et_al
 % result_paths{1} = fullfile(results_folder,'DHondt_et_al_2025\DHondt_et_al_2025_v14.mat');
 
 % Cell array with legend name for each result
-legend_names = {'Reference result', 'Your first simulation'};
+legend_names = {'MTP along metatarsals', 'MTP in sagittal plane'};
 
 % Path to the folder where figures are saved
 figure_folder = results_folder;
@@ -61,12 +61,12 @@ figure_savename = 'MyFirstPredictiveSimulationFigure';
 % initilise the counter for dynamic indexing
 fig_count = 1;
 
-figure_settings(fig_count).name = 'all_angles';
-figure_settings(fig_count).dofs = {'all_coords'};
-figure_settings(fig_count).variables = {'Qs'};
-figure_settings(fig_count).savepath = fullfile(figure_folder,[figure_savename '_' figure_settings(fig_count).name]);
-figure_settings(fig_count).filetype = {};
-fig_count = fig_count+1;
+% figure_settings(fig_count).name = 'all_angles';
+% figure_settings(fig_count).dofs = {'all_coords'};
+% figure_settings(fig_count).variables = {'Qs'};
+% figure_settings(fig_count).savepath = fullfile(figure_folder,[figure_savename '_' figure_settings(fig_count).name]);
+% figure_settings(fig_count).filetype = {};
+% fig_count = fig_count+1;
 
 % figure_settings(fig_count).name = 'all_angles';
 % figure_settings(fig_count).dofs = {'all_coords'};
@@ -82,20 +82,20 @@ fig_count = fig_count+1;
 % figure_settings(fig_count).filetype = {};
 % fig_count = fig_count+1;
 
-figure_settings(fig_count).name = 'all_lengths';
-figure_settings(fig_count).dofs = {'muscles_r'};
-figure_settings(fig_count).variables = {'lMtilde'};
-figure_settings(fig_count).savepath = fullfile(figure_folder,[figure_savename '_' figure_settings(fig_count).name]);
-figure_settings(fig_count).filetype = {};
-fig_count = fig_count+1;
-
-% figure_settings(fig_count).name = 'selected_angles';
-% figure_settings(fig_count).dofs = {'hip_flexion_r','hip_adduction_r','hip_rotation_r','knee_angle_r',...
-%     'ankle_angle_r','subtalar_angle_r','mtp_angle_r'};
-% figure_settings(fig_count).variables = {'Qs'};
+% figure_settings(fig_count).name = 'all_lengths';
+% figure_settings(fig_count).dofs = {'muscles_r'};
+% figure_settings(fig_count).variables = {'lMtilde'};
 % figure_settings(fig_count).savepath = fullfile(figure_folder,[figure_savename '_' figure_settings(fig_count).name]);
-% figure_settings(fig_count).filetype = {'jpeg'};
+% figure_settings(fig_count).filetype = {};
 % fig_count = fig_count+1;
+
+figure_settings(fig_count).name = 'selected_angles';
+figure_settings(fig_count).dofs = {'hip_flexion_r','hip_adduction_r','hip_rotation_r','knee_angle_r',...
+    'ankle_angle_r','subtalar_angle_r','mtj_angle_r','mtp_angle_r'};
+figure_settings(fig_count).variables = {'Qs'};
+figure_settings(fig_count).savepath = fullfile(figure_folder,[figure_savename '_' figure_settings(fig_count).name]);
+figure_settings(fig_count).filetype = {'jpeg'};
+fig_count = fig_count+1;
 
 % figure_settings(fig_count).name = 'torques';
 % figure_settings(fig_count).dofs = {'all_coords'};
@@ -104,12 +104,12 @@ fig_count = fig_count+1;
 % figure_settings(fig_count).filetype = {};
 % fig_count = fig_count+1;
 
-figure_settings(fig_count).name = 'limit torques';
-figure_settings(fig_count).dofs = {'all_coords'};
-figure_settings(fig_count).variables = {'T_limit'};
-figure_settings(fig_count).savepath = fullfile(figure_folder,[figure_savename '_' figure_settings(fig_count).name]);
-figure_settings(fig_count).filetype = {};
-fig_count = fig_count+1;
+% figure_settings(fig_count).name = 'limit torques';
+% figure_settings(fig_count).dofs = {'all_coords'};
+% figure_settings(fig_count).variables = {'T_limit'};
+% figure_settings(fig_count).savepath = fullfile(figure_folder,[figure_savename '_' figure_settings(fig_count).name]);
+% figure_settings(fig_count).filetype = {};
+% fig_count = fig_count+1;
 
 % figure_settings(fig_count).name = 'ankle_muscles';
 % figure_settings(fig_count).dofs = {'soleus_r','med_gas_r','lat_gas_r','tib_ant_r'};
