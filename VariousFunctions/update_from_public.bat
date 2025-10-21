d380257312c9e4d651331db150fa076a66ae88ec
Lars D'Hondt <71920801+Lars-DHondt-KUL@users.noreply.github.com>
2025-10-21 11:50:01 +0200

    clean up update_from_public.bat

--- VariousFunctions/update_from_public.bat
+++ VariousFunctions/update_from_public.bat
@@ -1,5 +1,4 @@
 @echo off
-
 rem +-------------------------------------------------------------------------------
 rem | update_from_public.bat
 rem |	This script updates the master branch of a (private) fork of the PredSim
@@ -17,36 +16,48 @@ rem | Original author: Lars D'Hondt
 rem | Original date: 13/August/2025
 rem +-------------------------------------------------------------------------------
 
+rem +-------------------------------------------------------------------------------
+rem | Defaults
+rem | These do not need to be changed if your (private) fork is set up according to 
+rem | the instructions in PredSim/Documentation/PrivateForkPredSim.md
+
+rem | Name and url of the remote.
+set PUBLIC_REPO_REMOTE_NAME=upstream
+set PUBLIC_REPO_REMOTE_URL=https://github.com/KULeuvenNeuromechanics/PredSim.git
+
+rem | Branch in the private repository that you want to update.
+set PRIVATE_REPO_MASTER_BRANCH=master
+
+
+
+rem +-------------------------------------------------------------------------------
+rem | Show info in command window to inform user what is going on.
 echo Running update_from_public.bat
 echo.
-echo This script updates the master branch of a (private) fork of the PredSim repository 
-echo based on the master branch of the public PredSim repository. 
+echo This script updates the %PRIVATE_REPO_MASTER_BRANCH% branch of a (private) fork
+echo of the PredSim repository based on the master branch of the public PredSim repo.
 echo For more information, see PredSim/Documentation/PrivateForkPredSim.md.
 echo.
 
 
-rem % Go to PredSim/ and run `echo git remote -v` on the command line to list the online  
-rem % repos linked to the local repo. For each line returned by this command, get the 
-rem % first 3 words (separated by spaces or tabs). These are then used to determine whether 
-rem % the current repo is a fork of the public PredSim repo, and whether the public repo
-rem % is linked as upstream.
+rem +-------------------------------------------------------------------------------
+rem | Detect how the repositories are set up.
 
-cd ..
-
-rem % Default name and url of the remote
-set PUBLIC_REPO_REMOTE_NAME=upstream
-set PUBLIC_REPO_REMOTE_URL=https://github.com/KULeuvenNeuromechanics/PredSim.git
-
+rem | Go to PredSim/ and run `echo git remote -v` on the command line to list the
+rem | online repos linked to the local repo. For each line returned by this command,
+rem | get the  first 3 words (separated by spaces or tabs). These are then used to 
+rem | determine whether the current repo is a fork of the public PredSim repo, and
+rem | whether the public repo is linked as upstream.
 
 set ORIGIN_URL=%PUBLIC_REPO_REMOTE_URL%
 set FOUND_PUBLIC_REPO_REMOTE="false"
-set IS_FORK="false"
 
+cd ..
 for /F "tokens=1,2,3" %%i in ('git remote -v') do (
 	
 	if "%%i"=="origin" (
 		set ORIGIN_URL=%%j
-	)
+	)	
 	
 	if "%%i %%j %%k"=="%PUBLIC_REPO_REMOTE_NAME% %PUBLIC_REPO_REMOTE_URL% (fetch)" (
 		set FOUND_PUBLIC_REPO_REMOTE="true"
@@ -54,48 +65,55 @@ for /F "tokens=1,2,3" %%i in ('git remote -v') do (
 )
 
 
-rem % Forked repos have a different url than the main PredSim repo.
-if not("%PUBLIC_REPO_REMOTE_URL%"=="%ORIGIN_URL%") (set IS_FORK="true")
+rem | Check whether the origin is a fork of the remote by comparing their url.
+if %PUBLIC_REPO_REMOTE_URL%==%ORIGIN_URL% (
+	set IS_FORK="false"
+) else (
+	set IS_FORK="true"
+)
 
-rem % Finish setting up the remote, if that wasn't done yet.
+
+rem | Finish setting up the remote, if that wasn't done yet.
 if %IS_FORK%=="true" if not(%FOUND_PUBLIC_REPO_REMOTE%=="true") (
-	echo setting "%PUBLIC_REPO_REMOTE_URL%" as %PUBLIC_REPO_REMOTE_NAME% repository
-	echo git remote add %PUBLIC_REPO_REMOTE_NAME% %PUBLIC_REPO_REMOTE_URL%
+	echo Setting "%PUBLIC_REPO_REMOTE_URL%" as %PUBLIC_REPO_REMOTE_NAME% repository
+	git remote add %PUBLIC_REPO_REMOTE_NAME% %PUBLIC_REPO_REMOTE_URL%
 	echo.
 )
 
+rem +-------------------------------------------------------------------------------
+rem | Do what we actually wanted to do: update the local master branch.
 
-rem % Do what we actually wanted to do: update the local master branch.
 if %IS_FORK%=="true" (
-	rem % Intended use: this script is run from a local clone of a (private) fork of the public repo.
-
-	rem % Perform the steps described in PredSim/Documentation/PrivateForkPredSim.md#manual
-	rem % 1.
-	echo git fetch %PUBLIC_REPO_REMOTE_NAME%
-	rem % 2.
-	echo git checkout -B master_public
-	echo git push -u origin master_public
-	rem % 3.
-	echo git merge %PUBLIC_REPO_REMOTE_NAME%/master -m "update from public master"
-	rem % 4.
-	echo git push origin
-	rem % 5.
-	start %ORIGIN_URL:~0,-4%/compare/master...master_public
+	rem | Intended use: this script is run from a local clone of a (private) fork of the public repo.
+	
+	rem | Perform the steps described in PredSim/Documentation/PrivateForkPredSim.md#manual
+	rem | 1.
+	git fetch %PUBLIC_REPO_REMOTE_NAME%
+	rem | 2.
+	git checkout -B master_public %PRIVATE_REPO_MASTER_BRANCH%
+	git push -u origin master_public
+	rem | 3.
+	git merge %PUBLIC_REPO_REMOTE_NAME%/master -m "update from public master"
+	rem | 4.
+	git push origin
+	rem | 5.
+	echo.
+	echo.
+	echo Create a pull request to finish updating the local %PRIVATE_REPO_MASTER_BRANCH% branch.
+	start %ORIGIN_URL:~0,-4%/compare/%PRIVATE_REPO_MASTER_BRANCH%...master_public
 	
 ) else (
+	rem | Unintended use: this script is run from a local clone of the public repo.
 
-	rem % Unintended use: this script is run from a local clone of the public repo.
-
-	rem % Rather than throwing an error or doing nothing, do what the (confused) user expects to happen:
-	rem % Update the local master branch baed on the master branch of the public repo.
-	echo git fetch origin
-	echo git checkout master
-	echo git pull origin
+	rem | Rather than throwing an error or doing nothing, do what the (confused) user expects to happen:
+	rem | Update the local master branch based on the master branch of the public repo.
+	git fetch origin
+	git checkout master
+	git pull origin
+	echo.
+	echo Done
 )
 
-rem % The End
-echo Done
-echo.
 
-rem % Keep the command window open until user closes it
+rem | Keep the command window open until user closes it
 pause
